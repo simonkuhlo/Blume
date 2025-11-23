8a1043d01f8a33e96c498f8b39d25991b908a244
simonkuhlo <siga241@gmail.com>
2025-11-23 21:52:46 +0100

    create frontend app

--- api/app.py
+++ api/app.py
@@ -4,13 +4,24 @@ from routes.crud.users import router as users_router
 from routes.crud.questions import router as questions_router
 from routes.crud.question_collections import router as question_collections_router
 from routes.crud.answers import router as answers_router
+from fastapi.middleware.cors import CORSMiddleware
 
 title: str = "Blume"
 summary: str = ""
 description: str = ""
 version: str = "0.0.1"
-
+origins = [
+    "http://localhost:6969",
+]
 app = FastAPI(title=title, summary=summary, description=description, version=version)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # Allowed origins
+    allow_credentials=True,       # Allow cookies and authentication headers
+    allow_methods=["*"],          # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],          # Allow all headers
+)
 app.include_router(entries_router)
 app.include_router(users_router)
 app.include_router(questions_router)
---
+++ frontend/app.py
@@ -0,0 +1,56 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import FileResponse
+
+title: str = "BlumeFrontend"
+summary: str = ""
+description: str = ""
+version: str = "0.0.1"
+origins = [
+    "http://localhost:8081",
+    "http://localhost:6969",
+]
+
+app = FastAPI(title=title, summary=summary, description=description, version=version)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # Allowed origins
+    allow_credentials=True,       # Allow cookies and authentication headers
+    allow_methods=["*"],          # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],          # Allow all headers
+)
+
+# Mount static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Configure Jinja2 templates directory
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.j2", {"request": request})
+
+@app.get("/test", response_class=HTMLResponse)
+async def partial(request: Request):
+    return templates.TemplateResponse("partial.j2", {"request": request})
+
+@app.get("/dedication", response_class=HTMLResponse)
+async def partial(request: Request):
+    return templates.TemplateResponse("dedication.j2", {"request": request})
+
+@app.get("/viewer", response_class=HTMLResponse)
+async def viewer(request: Request):
+    return templates.TemplateResponse("viewer.j2", {"request": request})
+
+@app.get("/creator", response_class=HTMLResponse)
+async def viewer(request: Request):
+    return templates.TemplateResponse("creator.j2", {"request": request})
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse("static/img/favicon.ico")
